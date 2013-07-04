b6629c123d8c74530e7a683449cc5fd5616bdedf
rgrannell1 <r.grannell1@nuigalway.ie>
2013-07-04 14:50:52 +0100

    working on a better  test for report

--- inst/tests/test-report.R
+++ inst/tests/test-report.R
@@ -1,32 +1,88 @@
 
-context("verify that invoking tests on report$ directly work")
+context(
+	"error functions on report$ can be invoked directly"
+)
 
-test_that("if all tests work correctly, then the values for pcall and test
-	should be seen in the ouput (rather than intermediate errors)", {
+pick_one <- function (x) 
+	sample(x, size = 1)
 
-	pcall <- 'I am le call'
-	test <- 'dummy'
+pick_len <- function (n)
+	seq_len(pick_one( seq_len(n) ))
 
-	pattern <- 'I am le call.+dummy'
+word_gen <- Generator(
+	function () {
 
-	expect_error(report$missing_traits(pcall), pcall)
-	expect_error(report$missing_value(pcall), pcall)
-	
-	expect_error(
-		report$traits_not_character(pcall, test),
-		pcall)
-	
-	expect_error(
-		report$invalid_traits(pcall, test),
-		pattern)
+		paste0(
+			sample(letters, size = pick_one(1:26)),
+			collapse = '' )
+	}
+)
 
-	expect_error(
-		report$non_boolean(pcall, list(test, test), 
-			pattern)
-	)
-	expect_error(
-		report$no_match(pcall, test, test),
-		pattern
-	)
+forall(
+	"error functions don't throw errors themselved (call is always displayed)",
+	list(
+		pcall  = word_gen,
+		traits = ,
+		invalid = ,
+		inputs = ,
+		actual = ,
+		value = ,
+		error = ,
+		inputs = 
+	),
+	function (pcall, traits, invalid, inputs, actual, value
+		error, inputs) {
+		# check that each of the expected fields are being 
+		# displayed in the output (won't happen in function is broken).
 
-})
+		all_patterns <- function (x) {
+			# get every permutation of elements in x,
+			# collapse them with |.
+
+			permutations <- sapply(
+				combinat::permn(x),
+				function (perm) {
+					paste0(perm, collapse = ".+")
+				})
+
+			paste0(permutations, collapse = "|")
+		}
+
+		expect_error(
+			report$missing_traits(pcall),
+			pcall)
+
+		expect_error(
+			report$missing_value(pcall),
+			pcall)
+
+		expect_error(
+			report$traits_not_character(pcall, traits),
+			all_patterns(pcall, traits))
+
+		expect_error(
+			report$invalid_traits(pcall, invalid),
+			all_patterns(pcall, invalid))
+
+		expect_error(
+			report$non_boolean(pcall, inputs, actual),
+			all_patterns(pcall, inputs$value, inputs$trait, actual))
+
+		expect_error(
+			report$no_match(pcall, value, traits),
+			all_patterns(pcall, value, traits))
+
+		expect_error(
+			report$error_encountered(pcall, error, inputs),
+			all_patterns(
+				pcall, error,
+				inputs$value, inputs$trait))
+
+		expect_error(
+			report$warning_encountered(pcall, error, inputs),
+			all_patterns(
+				pcall, error,
+				inputs$value, inputs$trait))
+
+	}
+)
